81ec7eb9bc3b7fd8f411e54351850a6b39aa6d82
Valentin Debon <valentin@debon.net>
2021-09-02 13:13:26 +0200

    Limited functionalities to markdown rendering

--- README.md
+++ README.md
@@ -1,71 +1,7 @@
-# Heaven
+# Haven
 
-This is a collection of utilities to manage projects in a unix-like fashion.
-It contains:
+Collection of utilities to generate websites and documentations from markdown.
 
-- hvn : Frontend to other subcommands.
-- hvn-cfgen : Create a basic configure script, which you can then modify manually to suit your needs.
-- hvn-init : Add a new module to your sources, and creates a hello world if the language is supported.
-- hvn-mando : Create roff manpages from markdown. Requires libcmark.
-- hvn-mkgen : Create a Makefile.rules containing rules to create object files associated to filename's extensions.
-- hvn-pkgen : Execute a set of scripts inside an isolated environment.
-
-## Why?
-
-Usually, projects tend to use tools such as cmake, ninja or whatever to create Makefiles when cloning a project.
-The goal was to be able to minimize the work of a cloner who just wants to build things.
-To respect this goal, I had to create utilities which only created files for tools everybody have: shell and make.
-
-## How to use it?
-
-Let's say we want to create a `libfoo` we use in an executable named `bar`, all written in C.
-Basically, you create a project, you just mkdir in one place, maybe git init, and then:
-
-	hvn-init -e .c libfoo bar
-
-Here, we tell `hvn-init` to create two modules, libfoo will be a library because of its prefix.
-And bar an executable because it doesn't have the lib prefix. All in the default sources location, `src/`
-We specify `.c` for the main file created in executables modules.
-
-Ok, now let's say you've coded a few things in `src/libfoo/` and `src/bar/`, you want to build that. First, create a configuration file:
-
-	hvn-cfgen -lC
-
-You now have a `configure` shell script in your current directory.
-You can add custom linking rules at the macro created exclusively for each module, I'll let you read the script to tweak it.
-The script is meant to take a `Makefile.rules` skeleton to generate a `Makefile`.
-The `-lC` option tells you want the script to look up for a CC compiler options. You can also ask for `C++` or both if you wanted.
-
-Then, we create the `Makefile.rules` using `hvn-mkgen`:
-
-	hvn-mkgen -dbar=libfoo
-
-The `-d` option specifies a list of comma separated dependencies for a module.
-
-Now, you can:
-
-	./configure
-
-Which will create `build/{bin,lib}` directories and a `Makefile`. In theory you should be able to run `make` just after that.
-But actually, under GNU/Linux, the linker doesn't look up for the crt's and compiler runtime. A fast workaround is to specify the compiler as the linker.
-The compiler-driver linker will certainly do the job:
-
-	LD=clang ./configure
-
-Then, you can `make` your project and you execute rules based on objects derived from your source tree.
-Try not to name two potentials object with the same basename, as the `.o` prefix __replaces__ the source file one.
-
-Yet the only one exempted from examples is certainly the most powerful: `hvn-pkgen`.
-This utility is linux-specific and enables creating containers to create a standard environment for executing/building/packaging.
-The main goal is to fake an environment where each build are ran in userspace but hiding the system headers and interfaces.
-It allows the package builder to easily change a toolchain or build scripts for distribution.
-I'll let you read the manual pages to understand how you can use it. You're only limited by your imagination.
-
-## Build
-
-Well, I'll let you figure it out from the previous section.
-
-## Install
-
-For this, you can manually copy the hierarchy under `build/` whenever your environment likes it.
+- hvn-man : Create groff manpages from markdown.
+- hvn-web : Create web html pages from markdown.
 
---
+++ src/hvn-web/main.c
@@ -0,0 +1,212 @@
+/*
+	main.c
+	Copyright (c) 2021, Valentin Debon
+
+	This file is part of the haven repository
+	subject the BSD 3-Clause License, see LICENSE
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <err.h>
+
+#include <cmark.h>
+
+struct hvn_web_args {
+	char *input, *output;
+	char *head, *tail;
+};
+
+struct hvn_web_render_option {
+	const char *name;
+	int value;
+};
+
+static const struct hvn_web_render_option renderoptions[] = {
+	{ "sourcepos", CMARK_OPT_SOURCEPOS },
+	{ "hardbreaks", CMARK_OPT_HARDBREAKS },
+	{ "nobreaks", CMARK_OPT_NOBREAKS },
+	{ "smart", CMARK_OPT_SMART },
+	{ "safe", CMARK_OPT_SAFE },
+	{ "unsafe", CMARK_OPT_UNSAFE },
+	{ "validate-utf8", CMARK_OPT_VALIDATE_UTF8 },
+};
+
+static void
+hvn_web_parse(int fd, cmark_parser *parser) {
+	const int pagesize = getpagesize();
+	char buffer[pagesize];
+	ssize_t readval;
+
+	while(readval = read(fd, buffer, sizeof(buffer)), readval > 0) {
+		cmark_parser_feed(parser, buffer, readval);
+	}
+
+	if(readval < 0) {
+		perror("read");
+	}
+}
+
+static void
+hvn_web_dump(const char *filename, FILE *output, const struct hvn_web_args *args) {
+	if(filename != NULL) {
+		int fd = open(filename, O_RDONLY);
+
+		if(fd >= 0) {
+			const int pagesize = getpagesize();
+			char buffer[pagesize];
+			ssize_t readval;
+
+			while(readval = read(fd, buffer, sizeof(buffer)), readval > 0) {
+				fwrite(buffer, sizeof(*buffer), readval, output);
+			}
+
+			close(fd);
+		} else {
+			warn("open %s", filename);
+		}
+	}
+}
+
+static void
+hvn_web_render(FILE *output, cmark_node *document, int options, const struct hvn_web_args *args) {
+	hvn_web_dump(args->head, output, args);
+
+	char *result = cmark_render_html(document, options);
+
+	fputs(result, output);
+
+	free(result);
+
+	hvn_web_dump(args->tail, output, args);
+}
+
+static void
+hvn_web_usage(const char *progname) {
+	fprintf(stderr, "usage: %s [-i <input>] [-o <output>] [-H <head>] [-T <tail>] [<render options>...]\n", progname);
+	exit(EXIT_FAILURE);
+}
+
+static const struct hvn_web_args
+hvn_web_parse_args(int argc, char **argv) {
+	struct hvn_web_args args = {
+		.input = NULL, .output = NULL,
+		.head = NULL, .tail = NULL,
+	};
+	int c;
+
+	while(c = getopt(argc, argv, ":i:o:H:T:"), c != -1) {
+		switch(c) {
+		case 'i':
+			free(args.input);
+			args.input = strdup(optarg);
+			break;
+		case 'o':
+			free(args.output);
+			args.output = strdup(optarg);
+			break;
+		case 'H':
+			free(args.head);
+			args.head = strdup(optarg);
+			break;
+		case 'T':
+			free(args.tail);
+			args.tail = strdup(optarg);
+			break;
+		case ':':
+			warnx("-%c: Missing argument", optopt);
+			hvn_web_usage(*argv);
+		default:
+			warnx("Unknown argument -%c", optopt);
+			hvn_web_usage(*argv);
+		}
+	}
+
+	/* Set output name from input's if available */
+	if(args.input != NULL && args.output == NULL) {
+		static const char htmlext[] = ".html";
+		const char *filename = basename(args.input);
+		const char *extension = strrchr(filename, '.');
+		const size_t length = extension != NULL ?
+			extension - filename : strlen(filename);
+		char buffer[length + sizeof(htmlext)];
+
+		strncpy(buffer, filename, length);
+		strncpy(buffer + length, htmlext, sizeof(htmlext));
+
+		args.output = strdup(buffer);
+	}
+
+	return args;
+}
+
+int
+main(int argc, char **argv) {
+	const struct hvn_web_args args = hvn_web_parse_args(argc, argv);
+	char **argpos = argv + optind, ** const argend = argv + argc;
+	int options = CMARK_OPT_DEFAULT;
+
+	/* Parse render options */
+	while(argpos != argend) {
+		const struct hvn_web_render_option *current = renderoptions,
+			* const end = renderoptions + sizeof(renderoptions) / sizeof(*renderoptions);
+		const char *name = *argpos;
+
+		while(current != end && strcmp(current->name, name) != 0) {
+			current++;
+		}
+
+		if(current != end) {
+			options |= current->value;
+		}
+
+		argpos++;
+	}
+
+	/* Parse input file */
+	cmark_parser *parser = cmark_parser_new(options);
+	if(args.input != NULL) {
+		int fd = open(args.input, O_RDONLY);
+
+		if(fd < 0) {
+			err(EXIT_FAILURE, "open %s", args.input);
+		}
+
+		hvn_web_parse(fd, parser);
+
+		close(fd);
+	} else {
+		hvn_web_parse(STDIN_FILENO, parser);
+	}
+
+	/* Print output file */
+	cmark_node *document = cmark_parser_finish(parser);
+	cmark_parser_free(parser);
+
+	if(args.output != NULL) {
+		FILE *output = fopen(args.output, "w");
+
+		if(output == NULL) {
+			err(EXIT_FAILURE, "fopen %s", args.output);
+		}
+
+		hvn_web_render(output, document, options, &args);
+
+		fclose(output);
+	} else {
+		hvn_web_render(stdout, document, options, &args);
+	}
+
+	cmark_node_free(document);
+	free(args.tail);
+	free(args.head);
+	free(args.output);
+	free(args.input);
+
+	return EXIT_SUCCESS;
+}
+
